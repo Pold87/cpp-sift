ecb7ddc603bbeff0d6b1e11732f93368dc398f20
pold87 <volker.strobel87@gmail.com>
2015-09-22 22:04:01 +0200

    Added missing files

--- src/test1.cpp
+++ src/test1.cpp
@@ -6,13 +6,15 @@ using namespace std;
 using namespace GeographicLib;
 
 int main() {
-  //Geocentric earth(Constants::WGS84_a(), Constants::WGS84_f());
-    Geocentric earth();
+
+  
+  
+  Geocentric earth(Constants::WGS84_a(), Constants::WGS84_f());
     
-      // Sample forward calculation (lat_lon -> ECEF)
-      double lat = 27.99, lon = 86.93, h = 8820; // Mt Everest
-      double ecef_x, ecef_y, ecef_z;
-      earth.Forward(lat, lon, h, ecef_x, ecef_y, ecef_z);
-      
+  // Sample forward calculation (lat_lon -> ECEF)
+  double lat = 27.99, lon = 86.93, h = 8820; // Mt Everest
+  double ecef_x, ecef_y, ecef_z;
+  earth.Forward(lat, lon, h, ecef_x, ecef_y, ecef_z);
+  
   return 0;
 }
